6cd3117dfa8a7d9cdfb1879b9c814f2022ecfd44
Joao-Salazar <joaopedrosalazar@gmail.com>
2022-03-29 23:41:10 +0100

    Functionality improvements
    App: Added variables to optimise the data being passed into components
    Results: Added conditional renderings to improve user experience
    Status: Added conditional renderings to improve user experience
    Card:Added conditional renderings to improve results reliability and cover matching to mimic Open Library.

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,20 +1,25 @@
 import Card from './Card'
 import TransitionEffect from '../utils/TransitionEffect'
 
-export default function Results({ data, loading }) {
+export default function Results({ booksList, loading }) {
 
-  if (!data) {
+  /** NOTE: Message for initial render when no search as been submitted */
+  if (!booksList) {
     return <p className="text-center text-gray-400">Search results will be displayed here.</p>
+    /** NOTE: Message for searches that didn't find results  */
+  } else if (Object.keys(booksList).length < 1) {
+    return <p className="text-center text-gray-400">No matches found for this search.</p>
   }
 
   return (
     <div className="grid grid-cols-1 gap-4 xl:grid-cols-5 lg:grid-col-4 md:grid-cols-3 sm:grid-cols-2">
-      {data.map((book, index) => (
+      {booksList.map((book, index) => (
         <TransitionEffect key={index} status={loading}>
           <Card
             title={book.title}
             author={book.author_name}
             cover={book.lending_edition_s}
+            coverAlt={book.cover_edition_key}
             published={book.first_publish_year}
           />
         </TransitionEffect>
